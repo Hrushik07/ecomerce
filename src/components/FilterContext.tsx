61c8bf9199140cf8c22b24e6d89de0d78698e7de
Hrushik.017 <hrushik.017@gmail.com>
2025-04-23 16:31:01 +0530

    deleted some unnecessary things and refined the code

--- src/components/FilterContext.tsx
+++ src/components/FilterContext.tsx
@@ -13,38 +13,40 @@ interface FilterContextType {
   setKeyword: (keyword: string) => void;
 }
 
-const FilterContext=createContext <FilterContextType | undefined>(undefined)
+const FilterContext = createContext<FilterContextType | undefined>(undefined);
 
-export const FilterProvider:React.FC<{children:React.ReactNode}> = ({children}) => {
-    const [searchQuery, setSearchQuery] = useState<string> ('')
-    const [selectedCategory, setSelectedCategory] = useState<string>('')
-    const [minPrice, setMinPrice] = useState<number | undefined>(undefined)
-    const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined)
-    const [keyword, setKeyword] = useState<string>("")
-selectedCategory
-    return (
-      <FilterContext.Provider
-        value={{
-          searchQuery,
-          setSearchQuery,
-          selectedCategory,
-          setSelectedCategory,
-          minPrice,
-          setMinPrice,
-          maxPrice,
-          setMaxPrice,
-          keyword,
-          setKeyword,
-        }}
-      >
-        {children}
-      </FilterContext.Provider>
-    );
-}
+export const FilterProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
+  const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
+  const [keyword, setKeyword] = useState<string>("");
+  selectedCategory;
+  return (
+    <FilterContext.Provider
+      value={{
+        searchQuery,
+        setSearchQuery,
+        selectedCategory,
+        setSelectedCategory,
+        minPrice,
+        setMinPrice,
+        maxPrice,
+        setMaxPrice,
+        keyword,
+        setKeyword,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+};
 export const useFilter = () => {
-    const context = useContext(FilterContext)
-    if (context === undefined) {
-        throw new Error("useFilterContext must be used within a FilterProvider")
-    }
-    return context
-}
+  const context = useContext(FilterContext);
+  if (context === undefined) {
+    throw new Error("useFilterContext must be used within a FilterProvider");
+  }
+  return context;
+};
